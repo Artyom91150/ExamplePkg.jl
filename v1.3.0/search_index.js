26f6a8aab60ab14b2f0d9f84467b0349a1eb4bee
Documenter.jl <documenter@juliadocs.github.io>
2024-01-17 10:12:56 +0000

    build based on fa649d3

---
+++ v1.3.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ExamplePkg","category":"page"},{"location":"#ExamplePkg","page":"Home","title":"ExamplePkg","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ExamplePkg.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ExamplePkg]","category":"page"}]
+}
